e2e47d677fde2b2d066da57198d0d9c71f962d7b
Jaechan An <jaechan@umd.edu>
2025-12-07 02:16:42 -0500

    Rename to cpp template

---
+++ template_cpp/main.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+   public:
+    int method(int inputs) {
+        return 0;
+    }
+};
+
+int main(int argc, char* argv[]) {
+    Solution sol;
+
+    int result = sol.method(0);
+
+    return 0;
+}
